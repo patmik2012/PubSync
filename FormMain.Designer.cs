3a4a9cc34154d92da06bdba6b882072c633ea0ae
Miklós Pataki <pataki.miklos@gmail.com>
2021-03-06 23:11:47 +0100

    DGFill, Hide CommandPrompt, rossz szerző

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -31,7 +31,7 @@ namespace PubSync
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lblMTMTCount = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblMTMTBooks = new System.Windows.Forms.Label();
             this.txtBxAuthor = new System.Windows.Forms.TextBox();
             this.lblAuthor = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
@@ -46,48 +46,51 @@ namespace PubSync
             // 
             this.groupBox1.AutoSize = true;
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
             this.groupBox1.Controls.Add(this.lblMTMTCount);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblMTMTBooks);
             this.groupBox1.Controls.Add(this.txtBxAuthor);
             this.groupBox1.Controls.Add(this.lblAuthor);
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(777, 134);
+            this.groupBox1.Size = new System.Drawing.Size(777, 105);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
             // lblMTMTCount
             // 
             this.lblMTMTCount.AutoSize = true;
-            this.lblMTMTCount.Location = new System.Drawing.Point(121, 104);
+            this.lblMTMTCount.Location = new System.Drawing.Point(111, 75);
             this.lblMTMTCount.Name = "lblMTMTCount";
             this.lblMTMTCount.Size = new System.Drawing.Size(0, 13);
             this.lblMTMTCount.TabIndex = 10;
             // 
-            // label1
+            // lblMTMTBooks
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 105);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "MTMT művek:";
+            this.lblMTMTBooks.AutoSize = true;
+            this.lblMTMTBooks.Location = new System.Drawing.Point(32, 76);
+            this.lblMTMTBooks.Name = "lblMTMTBooks";
+            this.lblMTMTBooks.Size = new System.Drawing.Size(77, 13);
+            this.lblMTMTBooks.TabIndex = 9;
+            this.lblMTMTBooks.Text = "MTMT művek:";
             // 
             // txtBxAuthor
             // 
-            this.txtBxAuthor.Location = new System.Drawing.Point(121, 59);
+            this.txtBxAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtBxAuthor.Location = new System.Drawing.Point(111, 30);
             this.txtBxAuthor.Name = "txtBxAuthor";
-            this.txtBxAuthor.Size = new System.Drawing.Size(100, 20);
-            this.txtBxAuthor.TabIndex = 8;
+            this.txtBxAuthor.Size = new System.Drawing.Size(147, 26);
+            this.txtBxAuthor.TabIndex = 0;
             this.txtBxAuthor.Text = "Süle Zoltán";
+            this.txtBxAuthor.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBxAuthor_KeyDown);
             // 
             // lblAuthor
             // 
             this.lblAuthor.AutoSize = true;
             this.lblAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblAuthor.Location = new System.Drawing.Point(38, 60);
+            this.lblAuthor.Location = new System.Drawing.Point(28, 31);
             this.lblAuthor.Name = "lblAuthor";
             this.lblAuthor.Size = new System.Drawing.Size(63, 20);
             this.lblAuthor.TabIndex = 7;
@@ -95,13 +98,14 @@ namespace PubSync
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(287, 57);
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSearch.Location = new System.Drawing.Point(313, 23);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnSearch.TabIndex = 6;
-            this.btnSearch.Text = "Keres";
+            this.btnSearch.Size = new System.Drawing.Size(102, 41);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "&Keres";
             this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // groupBox2
             // 
@@ -109,21 +113,25 @@ namespace PubSync
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.groupBox2.Controls.Add(this.dGVMTMT);
-            this.groupBox2.Location = new System.Drawing.Point(0, 140);
+            this.groupBox2.Location = new System.Drawing.Point(0, 111);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(777, 404);
+            this.groupBox2.Size = new System.Drawing.Size(777, 433);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             // 
             // dGVMTMT
             // 
+            this.dGVMTMT.AllowUserToAddRows = false;
+            this.dGVMTMT.AllowUserToDeleteRows = false;
             this.dGVMTMT.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
             this.dGVMTMT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dGVMTMT.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dGVMTMT.Location = new System.Drawing.Point(3, 16);
             this.dGVMTMT.Name = "dGVMTMT";
-            this.dGVMTMT.Size = new System.Drawing.Size(771, 385);
+            this.dGVMTMT.ReadOnly = true;
+            this.dGVMTMT.Size = new System.Drawing.Size(771, 414);
             this.dGVMTMT.TabIndex = 2;
             // 
             // FormMain
@@ -148,7 +156,7 @@ namespace PubSync
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lblMTMTCount;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMTMTBooks;
         private System.Windows.Forms.TextBox txtBxAuthor;
         private System.Windows.Forms.Label lblAuthor;
         private System.Windows.Forms.Button btnSearch;
--- FormMain.cs
+++ FormMain.cs
@@ -15,22 +15,31 @@ namespace PubSync
 {
     public partial class FormMain : Form
     {
+        List<MTMTBook> lstMTMBooks = new List<MTMTBook>();
+
         public void MTMTSearch(string author, int delay)
         {
-            List<MTMTBook> lstMTMBooks = new List<MTMTBook>();
-
             //MTMT oldalról adatok letöltése
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("headless");
-            IWebDriver driverMTMT = new ChromeDriver(options);
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            IWebDriver driverMTMT = new ChromeDriver(driverService,options);
 
             driverMTMT.Navigate().GoToUrl("http://www.mtmt.hu");
             driverMTMT.FindElement(By.Name("searchfield")).SendKeys(author + OpenQA.Selenium.Keys.Enter);
             Thread.Sleep(delay);
-            
-            //rossz szerző keresés kezelése
 
-            driverMTMT.FindElement(By.PartialLinkText(author)).Click();
+            //rossz szerző keresés kezelése
+            try
+            {
+                driverMTMT.FindElement(By.PartialLinkText(author)).Click();
+            }
+            catch
+            {
+                MessageBox.Show("Nem találtam adatot!", "Hiba",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DataClear();
+            }
             Thread.Sleep(delay);
 
             IList<IWebElement> titles = driverMTMT.FindElements(By.XPath("//div[@class='title']"));
@@ -53,27 +62,43 @@ namespace PubSync
                 }
             }
             driverMTMT.Quit();
+        }
 
-            lblMTMTCount.Text = lstMTMBooks.Count.ToString()+" db";
-
-            //DataGrid inicializálása és feltöltése
+        //DataGrid inicializálása és feltöltése
+        public void DGFill()
+        { 
             dGVMTMT.ColumnCount = 5;
             dGVMTMT.Columns[0].Name = "id";
             dGVMTMT.Columns[1].Name = "authors";
             dGVMTMT.Columns[2].Name = "title";
             dGVMTMT.Columns[3].Name = "pubInfo";
             dGVMTMT.Columns[4].Name = "pubEnd";
-            dGVMTMT.Rows.Clear();
 
             if (lstMTMBooks.Count > 0)
             {
+                DataExist();
+                lblMTMTCount.Text = lstMTMBooks.Count.ToString() + " db";
                 for (int i = 0; i < lstMTMBooks.Count; i++)
                 {
-                    string[] row = new string[] { (i+1).ToString(), lstMTMBooks[i].authors, lstMTMBooks[i].title, lstMTMBooks[i].pubInfo, lstMTMBooks[i].pubEnd };
+                    string[] row = new string[] { (i + 1).ToString(), lstMTMBooks[i].authors, lstMTMBooks[i].title, lstMTMBooks[i].pubInfo, lstMTMBooks[i].pubEnd };
                     dGVMTMT.Rows.Add(row);
                 }
             }
             dGVMTMT.AutoResizeColumns();
+        }
+
+        public void DataClear()
+        {
+            lblMTMTBooks.Visible = false;
+            lblMTMTCount.Visible = false;
+            lstMTMBooks.Clear();
+            dGVMTMT.Rows.Clear();
+        }
+
+        public void DataExist()
+        {
+            lblMTMTBooks.Visible = true;
+            lblMTMTCount.Visible = true;
         }
 
 
@@ -81,13 +106,27 @@ namespace PubSync
         public FormMain()
         {
             InitializeComponent();
+            DataClear();
         }
 
 
         //Keresés gomb
-        private void btnSearch_Click_1(object sender, EventArgs e)
+        private void btnSearch_Click(object sender, EventArgs e)
         {
+            Cursor.Current = Cursors.WaitCursor;
+            DataClear();
             MTMTSearch(txtBxAuthor.Text, 300);
+            DGFill();
+            Cursor.Current = Cursors.Default;
+        }
+
+        //TextBoxban Enterrel is lenyomja a keresés gombot
+        private void txtBxAuthor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                btnSearch.PerformClick();
+            }
         }
     }
 }
